c7190071d4cdbf60995d6567a37f9ad159c162ed
lajti00 <lajti00@gmail.com>
2015-11-24 21:42:39 +0100

    beta version is ready
    We can make price calculations and prepare special offers

--- LajtiCodeKatas/LajtiCodeKatas/PriceCalculator.cs
+++ LajtiCodeKatas/LajtiCodeKatas/PriceCalculator.cs
@@ -11,12 +11,14 @@ namespace LajtiCodeKatas
         private readonly List<string> _itemList = new List<string>();
         public readonly List<double> _priceList = new List<double>();
         private readonly List<string> _unitList = new List<string>();
+        // no offer: 0; percentage offer: 1; dependence offer: 2;
+        private readonly List<int> _offerList = new List<int>();
 
         public bool IsEmpty
         {
-            get { return _itemList.Count == 0 & _priceList.Count == 0 & _unitList.Count == 0; }
+            get { return _itemList.Count == 0 & _priceList.Count == 0 & _unitList.Count == 0 & _offerList.Count == 0; }
         }
-        public void Push(string item, string unit, double price)
+        public void Push(string item, string unit, double price, int offer)
         {
             int index = -1;
             for (int i = 0; i < Count; i++)
@@ -29,7 +31,15 @@ namespace LajtiCodeKatas
             _itemList.Add(item);
             _unitList.Add(unit);
             _priceList.Add(price);
+            _offerList.Add(offer);
+        }
+        public void Modify(string item, string unit, double price, int offer)
+        {
 
+            var index = searchItem(item);
+            _unitList[index] = unit;
+            _priceList[index] = price;
+            _offerList[index] = offer;
         }
         public int Count
         {
@@ -52,21 +62,77 @@ namespace LajtiCodeKatas
                     index = i;
                 }
             }
-            if(index == -1) throw new InvalidOperationException("The list does not contain the"+ item +"+item!!!");
+            if(index == -1) throw new InvalidOperationException("The list does not contain the"+ item +" item!!!");
             return index;
         }
-        public void PercentageOffer(DateTime startTime, DateTime endTime, string item, double percentageDiscount)
+        public void PercentageOffer(DateTime startTime, DateTime endTime, string item, double percentageDiscount, string[] wishesArray)
         {
             if (startTime < System.DateTime.Now & endTime > System.DateTime.Now)
             {
-                var i = searchItem(item);
-                _priceList[i] = (1-percentageDiscount / 100) * _priceList[i];
+                for(int j = 0; j < wishesArray.Length / 2; j++)
+                {
+                    if(wishesArray[j * 2] == item)
+                    {
+                        var i = searchItem(item);
+                        _priceList[i] = (1 - percentageDiscount / 100) * _priceList[i];
+                        Console.WriteLine(_itemList[i] + " has " + percentageDiscount.ToString() + "% discount ");
+                        _offerList[i] = 1;
+                    }
+                }
+
+
             }
 
         }
+        public void DependenceOffer(DateTime startTime, DateTime endTime, string item, string itemDependence, int dependenceCount, double percentageDiscount, string[] wishesArray)
+        {
+            if (startTime < System.DateTime.Now & endTime > System.DateTime.Now)
+            {
+                bool dep1 = false;
+                bool dep2 = false;
+                for (int j = 0; j < wishesArray.Length / 2; j++)
+                {
+                    if (wishesArray[j * 2] == item) {dep1 = true;}
+                    if (wishesArray[j * 2] == itemDependence & Convert.ToInt32(wishesArray[j * 2+1])>= dependenceCount) { dep2 = true; }
 
+                }
 
-        //TODO: Dependence offer, Delete item, Modify item, 
+                if (dep1  & dep2 )
+                {
+                    var i = searchItem(item);
+                    _priceList[i] = (1 - percentageDiscount / 100) * _priceList[i];
+                    Console.WriteLine(_itemList[i] + " has " + percentageDiscount.ToString() + "% discount ");
+                    _offerList[i] = 1;
+
+                } 
+            }
+
+        }
+        public void Calculator(string wishes)
+        {
+            //wishes "item,count,item,count ...."
+            var wishesArray = wishes.Split(',');
+            //offers
+            DateTime startDate = new DateTime(2015, 11, 2, 0, 0, 0);
+            DateTime endDate = new DateTime(2015, 11, 30, 0, 0, 0);
+            PercentageOffer(startDate, endDate, "Apple", 10, wishesArray);
+            DateTime startDate2 = new DateTime(2015, 11, 2, 0, 0, 0);
+            DateTime endDate2 = new DateTime(2015, 11, 30, 0, 0, 0);
+            DependenceOffer(startDate, endDate,"Bread", "Beans",2, 50, wishesArray);
+
+            int offer = 0;
+            for (int i = 0; i < wishesArray.Length/2; i++)
+            {
+                var item = searchItem(wishesArray[i * 2]);
+                var count = Convert.ToInt32(wishesArray[i * 2 + 1]);
+                double price = _priceList[item] * count;
+                Console.WriteLine(_itemList[item] + "\t" + count.ToString() + _unitList[item] + "\t" + "subtotal" + "\t" + price.ToString());
+                offer += _offerList[item];
+            }
+            if (offer == 0) { Console.WriteLine("(No offers available)"); }
+        }
+
+        //TODO:  Delete item, offer list to bool 
 
     }
 }
--- LajtiCodeKatas/LajtiCodeKatasUnitTests/PriceCalculatorTest.cs
+++ LajtiCodeKatas/LajtiCodeKatasUnitTests/PriceCalculatorTest.cs
@@ -21,7 +21,7 @@ namespace LajtiCodeKatasUnitTests
         public void IfIcreatePushAnItemToPriveCalcItDoesNotEmptyAnimore()
         {
             var priceCalc = new PriceCalculator();
-            priceCalc.Push("Apple", "bag", 1);
+            priceCalc.Push("Apple", "bag", 1, 0);
             Assert.That(priceCalc.IsEmpty, Is.False);
         }
 
@@ -29,50 +29,50 @@ namespace LajtiCodeKatasUnitTests
         public void IfIcreatePushAnItemToPriceCalcTheCountResultingTheNumberOfAddedItem()
         {
             var priceCalc = new PriceCalculator();
-            priceCalc.Push("Beans", "can", 0.65);
-            priceCalc.Push("Bread", "loaf", 0.80);
-            priceCalc.Push("Milk", "bottle", 1.30);
-            priceCalc.Push("Apple", "bag", 1);
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
             Assert.That(priceCalc.Count, Is.EqualTo(4));
         }
         [Test]
         public void IfIPushAnItemToPriceCalcWhichIsAlreadyPushedThePushReturnWithExeption()
         {
             var priceCalc = new PriceCalculator();
-            priceCalc.Push("Beans", "can", 0.65);
-            priceCalc.Push("Bread", "loaf", 0.80);
-            priceCalc.Push("Milk", "bottle", 1.30);
-            priceCalc.Push("Apple", "bag", 1);
-            Assert.Throws<InvalidOperationException>(() => priceCalc.Push("Apple", "bag", 2));
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
+            Assert.Throws<InvalidOperationException>(() => priceCalc.Push("Apple", "bag", 2, 0));
         }
         [Test]
         public void IfIcreatePushAnItemToPriceCalcTheListItemsWillListThem()
         {
             var priceCalc = new PriceCalculator();
-            priceCalc.Push("Beans", "can", 0.65);
-            priceCalc.Push("Bread", "loaf", 0.80);
-            priceCalc.Push("Milk", "bottle", 1.30);
-            priceCalc.Push("Apple", "bag", 1);
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
             priceCalc.ListItems();
         }
         [Test]
         public void IfsearcheAnItemTheSearchItemReturnTheCorrectIndex()
         {
             var priceCalc = new PriceCalculator();
-            priceCalc.Push("Beans", "can", 0.65);
-            priceCalc.Push("Bread", "loaf", 0.80);
-            priceCalc.Push("Milk", "bottle", 1.30);
-            priceCalc.Push("Apple", "bag", 1);
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
             Assert.That(priceCalc.searchItem("Milk"), Is.EqualTo(2));
         }
         [Test]
         public void IfsearcheAnItemWhickIsNotElementOfListTheSearchItemReturnExeption()
         {
             var priceCalc = new PriceCalculator();
-            priceCalc.Push("Beans", "can", 0.65);
-            priceCalc.Push("Bread", "loaf", 0.80);
-            priceCalc.Push("Milk", "bottle", 1.30);
-            priceCalc.Push("Apple", "bag", 1);
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
             Assert.Throws<InvalidOperationException>(() => priceCalc.searchItem("Milkk"));
         }
         [Test]
@@ -82,15 +82,52 @@ namespace LajtiCodeKatasUnitTests
             DateTime startDate = new DateTime(2015, 11, 2, 0, 0, 0);
             DateTime endDate = new DateTime(2015, 11, 30, 0, 0, 0);
 
-            priceCalc.Push("Beans", "can", 0.65);
-            priceCalc.Push("Bread", "loaf", 0.80);
-            priceCalc.Push("Milk", "bottle", 1.30);
-            priceCalc.Push("Apple", "bag", 1);
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
             priceCalc.ListItems();
-            priceCalc.PercentageOffer(startDate, endDate, "Apple", 10);
+            string[] wish = { "Apple", "1" };
+            priceCalc.PercentageOffer(startDate, endDate, "Apple", 10, wish);
             Console.WriteLine("AfterOffer");
             priceCalc.ListItems();
             Assert.That(priceCalc._priceList[3], Is.EqualTo(0.9));
+        }
+        public void IfIModifyAnItemToPriceCalcWhichIsNotPushedBeforeTheModifyReturnWithExeption()
+        {
+            var priceCalc = new PriceCalculator();
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            
+            Assert.Throws<InvalidOperationException>(() => priceCalc.Modify("Apple", "bag", 2, 0));
+        }
+        [Test]
+        public void IfIModifyAnItemToPriceCalcTheWaluesChange()
+        {
+            var priceCalc = new PriceCalculator();
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
+            priceCalc.Modify("Apple", "bag", 10, 0);
+            Assert.That(priceCalc._priceList[3], Is.EqualTo(10));
+
+        }
+        [Test]
+        public void Ifge()
+        {
+            var priceCalc = new PriceCalculator();
+            priceCalc.Push("Beans", "can", 0.65, 0);
+            priceCalc.Push("Bread", "loaf", 0.80, 0);
+            priceCalc.Push("Milk", "bottle", 1.30, 0);
+            priceCalc.Push("Apple", "bag", 1, 0);
+            priceCalc.Modify("Apple", "bag", 10, 0);
+
+            priceCalc.Calculator("Apple,2,Milk,3,Beans,2,Bread,1");
+
+            //Assert.That(priceCalc._priceList[3], Is.EqualTo(10));
+
         }
 
     }
